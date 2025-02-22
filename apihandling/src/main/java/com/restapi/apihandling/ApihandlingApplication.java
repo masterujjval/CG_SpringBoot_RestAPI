543f59f09faa9414b6c67c690d901e2746713cff
masterujjval <guptarickey3@gmail.com>
2025-02-22 21:50:38 +0530

    [Ujjwal Gupta] Add: Main branch added

---
+++ apihandling/src/main/java/com/restapi/apihandling/ApihandlingApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.apihandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApihandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApihandlingApplication.class, args);
+	}
+
+}
---
+++ apihandling/src/test/java/com/restapi/apihandling/ApihandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.apihandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApihandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
